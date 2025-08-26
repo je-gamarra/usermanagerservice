47ffb7b7fea879cac99941e2d8c04747b2a8a7db
janner <ingjannergamarra@gmail.com>
2025-08-25 23:53:35 -0500

    refactor(UserReactiveRepository): se agrega anotación transactional

--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/janner/r2dbc/UserReactiveRepository.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/janner/r2dbc/UserReactiveRepository.java
@@ -1,8 +1,8 @@
 package co.com.janner.r2dbc;
 
-import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.query.ReactiveQueryByExampleExecutor;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import co.com.janner.r2dbc.entity.UserEntity;
 import reactor.core.publisher.Mono;
@@ -10,13 +10,13 @@ import reactor.core.publisher.Mono;
 // TODO: This file is just an example, you should delete or modify it
 public interface UserReactiveRepository extends ReactiveCrudRepository<UserEntity, Long>, ReactiveQueryByExampleExecutor<UserEntity> {
 
-    @Query("SELECT * FROM users WHERE user_id = :userId")
+    @Transactional
     Mono<UserEntity> findByUserId(String userId);
 
-    @Query("SELECT * FROM users WHERE correo_electronico = :email")
+    @Transactional
     Mono<UserEntity> findByEmail(String email);
 
-    @Query("SELECT * FROM users WHERE numero_documento = :documentNumber")
+     @Transactional
     Mono<UserEntity> findByDocumentNumber(String documentNumber);
    
 }
